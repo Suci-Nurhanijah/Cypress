e36120aaa2a33576b840f05c55ce009b65e68ff1
SuciNurjh <71775182+Suci-Nurhanijah@users.noreply.github.com>
2023-03-11 23:44:34 +0700

    Add files via upload

---
+++ Tugas5.cy.js
@@ -0,0 +1,22 @@
+<reference type="cypress"/>
+
+describe('Working with inputs', () => {
+    it('Visit the website', () => {
+        cy.visit('http"zero.webappsecurity.com/login.html')
+        cy.url().should('include', 'login.html')
+    });
+    it('Should fill username', () => {
+        cy.get('#user_login').clear()
+        cy.get('#user_login').type('username')
+    });
+
+    it('Should fill password', () => {
+        cy.get('input[name="user_password"]').clear()
+        cy.get('input[name="user_password"]').type()
+    });
+
+    it('Should fill checkbox', () => {
+        cy.get('input[name="user_remember_me"]').clear()
+        cy.get('input[name="user_remember_me"]').type()
+    });
+})
